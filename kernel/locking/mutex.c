8fc6efe8749146f94863abdd2a56a086a18785c3
Peter Zijlstra <peterz@infradead.org>
2014-02-03 16:21:09 +0100

    locking/mutexes: Add extra reschedule point
    Add in an extra reschedule in an attempt to avoid getting reschedule
    the moment we've acquired the lock.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/n/tip-zah5eyn9gu7qlgwh9r6n2anc@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/locking/mutex.c
+++ kernel/locking/mutex.c
@@ -516,6 +516,13 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 		arch_mutex_cpu_relax();
 	}
 slowpath:
+	/*
+	 * If we fell out of the spin path because of need_resched(),
+	 * reschedule now, before we try-lock the mutex. This avoids getting
+	 * scheduled out right after we obtained the mutex.
+	 */
+	if (need_resched())
+		schedule_preempt_disabled();
 #endif
 	spin_lock_mutex(&lock->wait_lock, flags);
 
