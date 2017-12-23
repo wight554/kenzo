8bb053e35859d477acf6a3ff97a6b97a36aa1a50
Alex Naidis <alex.naidis@linux.com>
2017-12-23 21:37:40 +0530

    sched: Introduce Shadow scheduling
    Shadow scheduling is an extension for the
    HMP scheduler which provides efficient hotswitching
    between two scheduling policies.
    
    [...] (The documentation for this feature needs
           to be left out since it is property of
           Nextbit Systems Inc.)
    
    Configuration:
    
    sched_use_shadow_scheduling - controls whether shadow
                                  scheduling is enabled (ENABLED by Default)
    
    sched_shadow_active - internal variable for tracking
                          the shadow activity state
    
    [...]
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -25,6 +25,7 @@
 #include <linux/input.h>
 #include <linux/time.h>
 #include <linux/fsync.h>
+#include <linux/sched.h>
 
 struct cpu_sync {
 	int cpu;
@@ -195,6 +196,8 @@ static void do_input_boost_rem(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
+	sched_set_shadow_active(false);
+
 	if (sched_boost_active) {
 		ret = sched_set_boost(0);
 		if (ret)
@@ -227,6 +230,8 @@ static void do_input_boost(struct kthread_work *work)
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
+
+	sched_set_shadow_active(true);
 
 	/* Enable scheduler boost to migrate tasks to big cluster */
 	if (sched_boost_on_input) {
